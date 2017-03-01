d2171310109adc0cb180f03d227060227f508842
Sam Christiansen <boonezar@yahoo.com>
2017-02-28 19:22:19 -0700

    Add some navigation

--- src/cs3450/Main.java
+++ src/cs3450/Main.java
@@ -1,5 +1,6 @@
 package cs3450;
 
+import javax.swing.SwingUtilities;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -10,10 +11,10 @@ import javax.swing.JFrame;
 //import MainScreen;
 
 public class Main {
+	public static JFrame frame = new JFrame("Store Management System");
 
 	public static void startProgram()
 	{
-		JFrame frame = new JFrame("Store Management System");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setSize(new Dimension(1000,550));
@@ -22,9 +23,32 @@ public class Main {
 		frame.setVisible(true);
 	}
 
+	public static void showMainScreen()
+	{
+		frame.getContentPane().removeAll();
+		MainScreen.addComponentsToPane(frame.getContentPane());
+		updateFrame();
+	}
+
+	public static void showInventoryScreen()
+	{
+		frame.getContentPane().removeAll();
+		InventoryScreen.addComponentsToPane(frame.getContentPane());
+		updateFrame();
+	}
+
 	public static void main(String[] args) {
-		startProgram();
+		//System.out.print("start");
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				new Main().startProgram();
+		}});
+
 
 	}
 
+	public static void updateFrame(){
+		frame.getContentPane().repaint();
+		frame.getContentPane().revalidate();
+	}
 }
