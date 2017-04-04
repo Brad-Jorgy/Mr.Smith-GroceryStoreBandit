0671ae23dcef76d475b72de366f7581cc8cc4639
Sam Christiansen <boonezar@yahoo.com>
2017-04-04 16:08:51 -0600

    Fixed InitSQL so employee table is created correctly

--- src/cs3450/model/InitSQLite.java
+++ src/cs3450/model/InitSQLite.java
@@ -13,46 +13,34 @@ public class InitSQLite {
     public static Connection setupDatabaseIfNotSetup() {
         Connection connection = null;
         Statement statement = null;
+        File file = new File("cs3450.db");
+        if (!file.exists()) {
+            try {
+              connection = DriverManager.getConnection("jdbc:sqlite:cs3450.db");
+              statement = connection.createStatement();
+              statement.executeUpdate("create table if not exists inventory (itemId integer, name string, price double, quantity integer, provider string)");
+              DataAccess db = new XLSAdapter();
+              ArrayList<Product> products = db.loadAllProducts();
+              for (int i = 0; i < products.size(); i++)
+                statement.executeUpdate("insert into inventory values(" + products.get(i).getId() + ", '" + products.get(i).getName() + "', " + products.get(i).getPrice() + ", " + products.get(i).getQuantity() + ", '" + products.get(i).getProvider() + "')");
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:cs3450.db");
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
             statement.executeUpdate("create table if not exists inventory (itemId integer, name string, price double, quantity integer, provider string)");
-            //add other tables here.
-//            statement.executeUpdate("drop table orders");
-//            statement.executeUpdate("drop table customers");
             statement.executeUpdate("create table if not exists orders (orderId integer, itemId integer, quantity integer)");
             statement.executeUpdate("create table if not exists customers (customerId integer, orderId integer, name string, creditCard string, address string, city string, state string, zipcode string, country string)");
+            statement.executeUpdate("create table if not exists employees (employeeId integer, name string, image blob, username string, password string, position string)");
+            ResultSet rs = statement.executeQuery("select * from employees");
+            if(!rs.next())
+              statement.executeUpdate("insert into employees values(1, 'Default Manager', null, 'manager', '12345', 'Manager')");
         } catch (SQLException e) {
             System.err.println(e.getMessage());
-        }
-
-        File file = new File("cs3450.db");
-        if (!file.exists()) {
-            try {
-                if (connection == null) {
-                    connection = DriverManager.getConnection("jdbc:sqlite:cs3450.db");
-                } else {
-                    DataAccess db = new XLSAdapter();
-                    ArrayList<Product> products = db.loadAllProducts();
-                    for (int i = 0; i < products.size(); i++) {
-                        System.out.println("insert into inventory values(" + products.get(i).getId() + ", '" + products.get(i).getName() + "', " + products.get(i).getPrice() + ", " + products.get(i).getQuantity() + ", '" + products.get(i).getProvider() + "')");
-                        statement.executeUpdate("insert into inventory values(" + products.get(i).getId() + ", '" + products.get(i).getName() + "', " + products.get(i).getPrice() + ", " + products.get(i).getQuantity() + ", '" + products.get(i).getProvider() + "')");
-                        System.out.println("reached");
-                    }
-                }
-            } catch (SQLException e) {
-                System.err.println(e.getMessage());
-            } finally {
-                try {
-                    if (connection != null)
-                        connection.close();
-                } catch (SQLException e) {
-                    System.err.println(e.getMessage());
-                }
-            }
         }
         return connection;
     }
 }
-
