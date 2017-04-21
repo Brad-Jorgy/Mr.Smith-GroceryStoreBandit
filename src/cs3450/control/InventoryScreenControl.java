e013ebab8f4a9294da2c2d713e2379cef16c0403
Sam Christiansen <boonezar@yahoo.com>
2017-04-20 23:32:09 -0600

    Changed product to include discount price

--- src/cs3450/control/InventoryScreenControl.java
+++ src/cs3450/control/InventoryScreenControl.java
@@ -20,9 +20,10 @@ public class InventoryScreenControl{
 
   static public void showUpdateProductPopup(Product product){
       JPanel popupPanel = new JPanel();
-      popupPanel.setLayout(new GridLayout(5,2));
+      popupPanel.setLayout(new GridLayout(6,2));
       JTextField nameTF = new JTextField(product.getName());
       JTextField priceTF = new JTextField("" + product.getPrice(), 20);
+      JTextField discountPriceTF = new JTextField("" + product.getDiscountPrice(), 20);
       JTextField quantityTF = new JTextField("" + product.getQuantity(), 20);
       JTextField providerTF = new JTextField(product.getProvider(), 20);
       popupPanel.add(new JLabel("Id: "));
@@ -31,16 +32,19 @@ public class InventoryScreenControl{
       popupPanel.add(nameTF);
       popupPanel.add(new JLabel("Price($): "));
       popupPanel.add(priceTF);
+      popupPanel.add(new JLabel("Discount Price($): "));
+      popupPanel.add(discountPriceTF);
       popupPanel.add(new JLabel("Quantity: "));
       popupPanel.add(quantityTF);
       popupPanel.add(new JLabel("Provider: "));
       popupPanel.add(providerTF);
       int result = JOptionPane.showConfirmDialog(null, popupPanel, "Update Product:", JOptionPane.OK_CANCEL_OPTION);
       if(result ==JOptionPane.OK_OPTION){
-        if(areValuesValid(priceTF.getText(), quantityTF.getText())){
+        if(areValuesValid(priceTF.getText(), discountPriceTF.getText(), quantityTF.getText())){
           DataAccess db = Main.getSQLiteAccess();
           product.setName(nameTF.getText());
           product.setPrice(Double.parseDouble(priceTF.getText()));
+          product.setDiscountPrice(Double.parseDouble(discountPriceTF.getText()));
           product.setQuantity(Integer.parseInt(quantityTF.getText()));
           product.setProvider(providerTF.getText());
           db.saveProduct(product);
@@ -54,24 +58,27 @@ public class InventoryScreenControl{
 
     static public void showAddProductPopup(){
       JPanel popupPanel = new JPanel();
-      popupPanel.setLayout(new GridLayout(5,2));
+      popupPanel.setLayout(new GridLayout(6,2));
       JTextField nameTF = new JTextField();
       JTextField priceTF = new JTextField();
+      JTextField discountPriceTF = new JTextField();
       JTextField quantityTF = new JTextField();
       JTextField providerTF = new JTextField();
       popupPanel.add(new JLabel("Name: "));
       popupPanel.add(nameTF);
       popupPanel.add(new JLabel("Price($): "));
       popupPanel.add(priceTF);
+      popupPanel.add(new JLabel("Discount Price($): "));
+      popupPanel.add(discountPriceTF);
       popupPanel.add(new JLabel("Quantity: "));
       popupPanel.add(quantityTF);
       popupPanel.add(new JLabel("Provider: "));
       popupPanel.add(providerTF);
       int result = JOptionPane.showConfirmDialog(null, popupPanel, "Add Product:", JOptionPane.OK_CANCEL_OPTION);
       if(result ==JOptionPane.OK_OPTION){
-        if(areValuesValid(priceTF.getText(), quantityTF.getText())){
+        if(areValuesValid(priceTF.getText(), discountPriceTF.getText(), quantityTF.getText())){
           DataAccess db = Main.getSQLiteAccess();
-          Product product = new Product(9867, nameTF.getText(), Double.parseDouble(priceTF.getText()), Integer.parseInt(quantityTF.getText()), providerTF.getText());
+          Product product = new Product(9867, nameTF.getText(), Double.parseDouble(priceTF.getText()), Double.parseDouble(discountPriceTF.getText()), Integer.parseInt(quantityTF.getText()), providerTF.getText());
           db.saveNewProduct(product);
         }
         else{
@@ -83,17 +90,15 @@ public class InventoryScreenControl{
 
     static public void showDeleteProductPopup(Product product){
       JPanel popupPanel = new JPanel();
-       popupPanel.setLayout(new GridLayout(5,2));
-      // JTextField nameTF = new JTextField(product.getName());
-      // JTextField priceTF = new JTextField("" + product.getPrice(), 20);
-      // JTextField quantityTF = new JTextField("" + product.getQuantity(), 20);
-      // JTextField providerTF = new JTextField(product.getProvider(), 20);
+       popupPanel.setLayout(new GridLayout(6,2));
       popupPanel.add(new JLabel("Id: "));
       popupPanel.add(new JLabel("" + product.getId()));
       popupPanel.add(new JLabel("Name: "));
       popupPanel.add(new JLabel(product.getName()));
       popupPanel.add(new JLabel("Price($): "));
       popupPanel.add(new JLabel("" + product.getPrice()));
+      popupPanel.add(new JLabel("Discount Price($): "));
+      popupPanel.add(new JLabel("" + product.getDiscountPrice()));
       popupPanel.add(new JLabel("Quantity: "));
       popupPanel.add(new JLabel("" + product.getQuantity()));
       popupPanel.add(new JLabel("Provider: "));
@@ -106,9 +111,13 @@ public class InventoryScreenControl{
       updateInventoryScreen();
     }
 
-    static public boolean areValuesValid(String price, String quantity){
+    static public boolean areValuesValid(String price, String discountPrice, String quantity){
       if(!price.matches("[0-9]*.?[0-9]?[0-9]?")){
         System.out.println("Error: invalid price");
+        return false;
+      }
+      if(!discountPrice.matches("[0-9]*.?[0-9]?[0-9]?")){
+        System.out.println("Error: invalid discount price");
         return false;
       }
       if(!quantity.matches("[0-9]*")){
